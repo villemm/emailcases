1bc21a39d6cc51c7aa380b2685c4472735568bf8
ville.maanpaa <ville.maanpaa@cc7d5a41-502d-0410-800f-85b37150911e>
2007-04-05 04:55:56 +0000

    added created on and updated on dates to tasks

--- trunk/db/migrate/001_create_tasks.rb
+++ trunk/db/migrate/001_create_tasks.rb
@@ -4,6 +4,8 @@ class CreateTasks < ActiveRecord::Migration
 		t.column :name, :string
 		t.column :duration_id, :integer, :value => 1
 		t.column :person_id, :integer, :value => 1
+		t.column :updated_on, :timestamp
+		t.column :created_on, :timestamp
 	end
   end
 
